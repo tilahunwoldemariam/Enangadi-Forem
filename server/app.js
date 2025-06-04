76ae8c74b6f4b4cfc382662ffddc248992b60ffe
salimahmedmuhammed98765@gmail.com <salimahmedmuhammed98765@gmail.com>
2025-06-04 15:02:03 +0300

    feat(database): Implement MySQL schema and connection setup

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// Initialize Express
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test the backend listening
+app.get('/', (req, res) => {
+  res.send('Welcome to the Evangadi Forum API');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(`❌ Failed to start the server on port ${port}`);
+  }
+
+  console.log(`✅ Server is running on port http://localhost:${port}`);
+});
