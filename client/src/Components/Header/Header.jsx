094256362e080da8d1bf8243da533bb9b7183611
Hailu401 <123373063+Hailu401@users.noreply.github.com>
2025-06-09 19:24:16 +0300

    header-css done

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./Header.css";
-
+import logo from '../../asset/images/header_logo.png'
 const Header = ({ isAuthenticated, onSignOut }) => {
   return (
     <header className="header">
       <div className="logo">
-        <span className="logo-orange">E</span>
-        <span className="logo-black">NGADI</span>
+        <img src={logo} alt="evangadi_logo" />
       </div>
-      <nav className="nav-links">
+      <div className="nav-links">
         <a href="/">Home</a>
         <a href="/how-it-works">How it works</a>
-      </nav>
-      <div className="auth-button">
-        {isAuthenticated ? (
-          <button onClick={onSignOut} className="btn blue">
-            LOG OUT
-          </button>
-        ) : (
-          <a href="/signin" className="btn blue">
-            SIGN IN
-          </a>
-        )}
+
+        <div className="auth-button">
+          {isAuthenticated ? (
+            <button onClick={onSignOut} className="btn blue">
+              LOG OUT
+            </button>
+          ) : (
+            <a href="/signin" className="btn blue">
+              SIGN IN
+            </a>
+          )}
+        </div>
       </div>
     </header>
   );
