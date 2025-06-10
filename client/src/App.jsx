acc4763867963662a508ef33952e065bd83ed54d
try-gi <meaza.ketaw@yahoo.com>
2025-06-10 22:24:22 +0200

    footer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import LoginPage from './Pages/LoginPage/LoginPage';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      <Header isAuthenticated={false}/>
+      <Header isAuthenticated={false} />
       <LoginPage />
+      <Footer />
     </div>
   );
 }
