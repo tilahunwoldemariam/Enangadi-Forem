0580da128b610c2f4f32c997a352f6036b0aa2d2
12-selam <kidaneselam778@yahoo.co.uk>
2025-06-10 22:36:21 +0100

    about page done

--- client/src/Pages/LoginPage/LoginPage.jsx
+++ client/src/Pages/LoginPage/LoginPage.jsx
@@ -468,6 +468,34 @@ function LoginPage() {
           </div>
 
         </div>
+
+                  {/* Right Side */}
+        <div className={styles.right_side}>
+          <div className={styles.right_in_contener}>
+            <small className="about">About</small>
+            <h2
+              className={`${styles.evangadi_network} ${styles.text_gradients}`}
+            >
+              Evangadi Networks
+            </h2>
+
+            <p className={`${styles.p1} ${styles.infoParagraph}`}>
+              No matter what stage of life you are in, whether you’re just
+              starting elementary school or being promoted to CEO of a Fortune
+              500 company, you have much to offer to those who are trying to
+              follow in your footsteps.
+            </p>
+            <p className={`${styles.p2} ${styles.infoParagraph}`}>
+              Wheather you are willing to share your knowledge or you are just
+              looking to meet mentors of your own, please start by joining the
+              network here.
+            </p>
+
+            <button className={`${styles.h_w_butn} h_w_butn`}>
+              <Link to="/howItWork">HOW IT WORKS</Link>
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
