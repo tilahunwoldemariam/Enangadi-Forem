0f571115efc8b82f95cd90c6b4e64a4d381f09bb
salimahmedmuhammed98765@gmail.com <salimahmedmuhammed98765@gmail.com>
2025-06-12 21:59:04 +0300

    footer img change to original

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./Footer.css";
-import logo from "../../asset/evangadi-logo-footer.png";
-import { FaFacebookF } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaYoutube } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Footer.css';
+import logo from '../../asset/evangadi-logo-footer.png';
+import { FaFacebookF } from 'react-icons/fa6';
+import { FaInstagram } from 'react-icons/fa6';
+import { FaYoutube } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <section className="footer__container">
